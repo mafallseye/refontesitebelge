c2bf35eb4d24cd0087e34aaa26a50bcd8ba3544d
mafallseye <maguettefseye@gmail.com>
2023-10-20 18:27:18 +0200

    new commit

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -43,7 +43,47 @@
     </ul> -->
     </div>
 </footer>
-
+<button type="button" v-show="scY > 300"  @click="toTop" class=" fixed right-0 bottom-0 text-blue-700 border border-red-600 hover:bg-red-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800 dark:hover:bg-red -500">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3.5"
+                    stroke="currentColor" class="w-6 h-6 text-gray-900 hover:text-white">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                </svg>
+  <span class="sr-only">Icon description</span>
+</button>
 </div>
 
 </template>
+<script>
+export default {
+    data() {
+        return {
+            annee: new Date(),
+            scTimer: 0,
+        scY: 0,
+        }
+    },
+  
+        methods: {
+      handleScroll: function () {
+        if (this.scTimer) return;
+        this.scTimer = setTimeout(() => {
+          this.scY = window.scrollY;
+          clearTimeout(this.scTimer);
+          this.scTimer = 0;
+        }, 100);
+      },
+
+      toTop: function () {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      },
+    },
+        
+    mounted() {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+
+}
+</script>
