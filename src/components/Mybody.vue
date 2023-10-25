53f891dce0d41179f82a17e8c7cb754470e9629e
mafallseye <maguettefseye@gmail.com>
2023-10-25 10:46:32 +0200

    new commit

--- src/components/Mybody.vue
+++ src/components/Mybody.vue
@@ -94,7 +94,7 @@
       <div class="card-body">
         <h5 class="card-title text-center"> Programme FORAFRICC</h5>
         <p class="card-text text-center">Forum Africain des Industries Culturelles et créatives</p>
-        <button type="button" class="text-white float-end   focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center  dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+        <button type="button" class="text-white float-end mt-6  focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center  dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
   <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
   </svg>
