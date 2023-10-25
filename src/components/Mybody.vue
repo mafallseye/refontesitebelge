0fe73b67e54adac279fe770cab78d8cbc634ae51
mafallseye <maguettefseye@gmail.com>
2023-10-25 10:40:55 +0200

    new commit

--- src/components/Mybody.vue
+++ src/components/Mybody.vue
@@ -92,7 +92,7 @@
     <div class="card h-100 card6">
       <!-- <img src="..." class="card-img-top" alt="..."> -->
       <div class="card-body">
-        <h5 class="card-title text-center">Presentation programme FORAFRICC</h5>
+        <h5 class="card-title text-center"> Programme FORAFRICC</h5>
         <p class="card-text text-center">Forum Africain des Industries Culturelles et créatives</p>
         <button type="button" class="text-white float-end   focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center  dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
   <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
