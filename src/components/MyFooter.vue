a60d1694c5062178f07889ad492ccd17032c23ff
mafallseye <maguettefseye@gmail.com>
2023-10-25 20:55:22 +0200

    new commit

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -24,7 +24,7 @@
 </div>
 </div>
 
-<footer class="bg-white dark:bg-gray-900">
+<footer class="bg-white dark:bg-gray-900 mb-6 md:mb-0">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <!-- <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
