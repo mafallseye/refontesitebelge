f33dca594dbbfdea465d04f491e43e4e0fcce75f
mafallseye <maguettefseye@gmail.com>
2023-10-25 20:05:38 +0200

    new commit

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -26,15 +26,15 @@
 
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
+        <!-- <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
-              <!-- <a href="https://flowbite.com/" class="flex items-center">
+              <a href="https://flowbite.com/" class="flex items-center">
                   <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
                   <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-              </a> -->
+              </a>
           </div>
-          >
-      </div>
+          
+      </div> -->
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://hub.brussels/fr/" class="hover:underline">hub.brussels</a>.
