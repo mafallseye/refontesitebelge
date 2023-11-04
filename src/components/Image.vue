df6357bd3ffbb16534f356d08a28f0f25acf5483
mafallseye <maguettefseye@gmail.com>
2023-11-04 14:55:43 +0100

    new commit

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -4,8 +4,20 @@
         <img src="../assets/brussel.png" class="float-start" alt="...">
     <img src="../assets/senregal.png" class=" float-end" alt="...">
     </div> -->
-    <img class="h-auto w-full  mx-auto" src="../assets/bru-amb.png" alt="image description">
-
+    <!-- <img class="h-auto w-full  mx-auto" src="../assets/bru-amb.png" alt="image description"> -->
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../assets/bru-amb.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/bru-amb.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../assets/bru-amb.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+</div>
 </template>
 <style scoped>
 
